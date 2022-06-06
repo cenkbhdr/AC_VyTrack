cfb44e4146ccc2e511b0b0e8ab404f3a0ca4f224
CENK BAHADIR <bahadircenk2@gmail.com>
2022-06-06 01:01:28 -0400

    US-1 TC-1

--- src/test/java/com/vyTrack/test_Features/US_01_TC1_LoginFunctionality_AllCorrect.java
+++ src/test/java/com/vyTrack/test_Features/US_01_TC1_LoginFunctionality_AllCorrect.java
@@ -1,4 +1,27 @@
 package com.vyTrack.test_Features;
 
+import com.vyTrack.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+
 public class US_01_TC1_LoginFunctionality_AllCorrect {
+    /**  US_01 Login Functionality
+     *      TC_01 All Correct Credentials
+     *
+     * Given  at LoginPage
+     * When input correct username to usernameBox
+     * And input correct password to passwordBox
+     * And clicking on LoginBtn
+     * Then should navigate to DashboardPage
+     */
+    public static void main(String[] args) {
+
+
+        //1.
+        WebDriver driver = Driver.getWebDriver();
+
+        //2.Navigate to https://qa2.vytrack.com/user/login
+        driver.get("https://qa2.vytrack.com/user/login");
+
+
+    }
 }
